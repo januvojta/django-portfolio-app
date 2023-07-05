ef06543047b6b387841577cc9f330092334d1723
Vojta <januvojta@gmail.com>
2023-07-05 19:31:30 +0200

    add registration of users

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
+from users.forms import CustomUserCreationForm
+
+
+
 def dashboard(request):
-    return render(request, "dashboard.html")
+    return render(request, "users/dashboard.html")
+
+def register(request):
+    if request.method == "GET":
+        return render (request, "users/register.html", {"form": CustomUserCreationForm})
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "users/register_done.html", {"form": form})
+        else:
+            return render(request, "users/register.html", {"form": form})
